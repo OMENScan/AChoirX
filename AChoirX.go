094912dab03a928e8f782488eb4e9532e4b8bf05
OMENScan <OMENScan@gmail.com>
2020-08-21 19:42:27 -0700

    V0.001

---
+++ AChoirX.go
@@ -0,0 +1,20 @@
+// ****************************************************************
+// "As we benefit from the inventions of others, we should be
+//  glad to share our own...freely and gladly."
+//  - Benjamin Franklin
+//
+// AChoirX v0.01 - Convert from C to Go for Xplatform capability
+//
+// ****************************************************************
+
+package main
+
+import "fmt"
+
+func main() {
+    var Version = "v4.4"
+    var RunMode = "Run"
+
+    fmt.Println("AChoirX Version: ", Version)
+    fmt.Println("AChoirX RunMode: ", RunMode)
+}
