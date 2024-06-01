0f6a636db2f5766f0e3878d7a720a3f9e4675b6c
OMENScan <OMENScan@gmail.com>
2024-06-01 09:55:48 -0700

    Update FindDirs.ps1

--- NewIdeas/FindDirs.ps1
+++ NewIdeas/FindDirs.ps1
@@ -4,6 +4,10 @@ $DirName=$args[0]
 # Copy the IIS XML File to <Directory> so we have our own copy in the collection
 $SysRoot = $Env:SYSTEMROOT
 Copy-Item "$SysRoot\System32\inetsrv\config\applicationHost.config" -Destination "$DirName"
+# Start with some standard Locations
+Add-Content -Path "$DirName\DirOutput.txt" -Value "%SystemRoot%\System32\LogFiles"
+Add-Content -Path "$DirName\DirOutput.txt" -Value "%SystemDrive%\Windows.old\Windows\System32\LogFiles"
+Add-Content -Path "$DirName\DirOutput.txt" -Value "%SystemDrive%\inetpub\logs\LogFiles"
 # Use Powershell to parse the XPaths of the Log Locations
 Select-Xml -Path $DirName\applicationHost.config -XPath '/configuration/system.applicationHost/log/centralBinaryLogFile' | ForEach-Object { $_.Node.directory } | Add-Content -Path "$DirName\DirOutput.txt"
 Select-Xml -Path $DirName\applicationHost.config -XPath '/configuration/system.applicationHost/log/centralW3CLogFile' | ForEach-Object { $_.Node.directory } | Add-Content -Path "$DirName\DirOutput.txt"
